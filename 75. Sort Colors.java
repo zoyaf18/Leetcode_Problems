e4b2d8de2b2078576e4941207e5e187e42b482a4
Zoya Fatima <64043575+zoyaf18@users.noreply.github.com>
2023-06-19 19:15:16 +0530

    Create 75. Sort Colors.java

---
+++ 75. Sort Colors.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int temp=0;
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+
+        while(mid <= high){
+            switch(nums[mid]) {
+
+                case 0: {
+                    temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+
+                case 1: {
+                    mid++;
+                    break;
+                }
+
+                case 2: {
+                    temp = nums[high];
+                    nums[high] = nums[mid];
+                    nums[mid] = temp;
+                    high--;
+                    break;
+                }
+            }
+        }
+        
+    }
+}
